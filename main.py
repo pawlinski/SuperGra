46aa44af6d2bcc05441f498c931768d23fc97d09
rpawlinski <radoslaw.pawlinski@gmail.com>
2025-01-19 13:18:17 +0100

    lekcja 18

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
 import random
+import math
 
 from pygame.examples.go_over_there import clock
 
 # rozpocznij program
 pygame.init()
-clock = pygame.time.Clock() # zmienna zawiera zczytany czas systemowy
+
+score = 0
+# dirtyRect = [] # "kwadraty" które mają się odświeżać
+
+clockGame = pygame.time.Clock() # zmienna zawiera zczytany czas systemowy
 
 # stwórz ekran gry
 screen = pygame.display.set_mode((800, 600)) # szerokość i wysokość okna
@@ -23,30 +28,53 @@ playerX = 368 # całość ma (800) / 2 - szerokość obrazka  (64) / 2
 playerY = 480
 speedX = 0
 speedY = 0
+playerSpeedChange = 3
 
 # przeciwnik 1
 enemyImg = pygame.image.load("assets/ninja_64.png")
-enemyX = random.randint(0, 736)
-enemyY = 0 # pokaże się na górze ekranu
-enemySpeedX = random.choice([-3, -2, -1, 1, 2, 3]) # wybór prędkości i kierunku z listy (nie może być 0)
+enemyX = random.randint(1, 735)  # dla bezpieczeństwa zmieniamy z 0 na 1 i z 736 na 735
+enemyY = 0  # pokaże się na górze ekranu
+enemySpeedX = random.choice([-3, -2, -1, 1, 2, 3])  # wybór prędkości i kierunku z listy (nie może być 0)
 
 # strzał
 swordImg = pygame.image.load("assets/sword_32.png")
 swordX = 0
 swordY = 0
-swordSpeedY = 3
+swordSpeedY = 5
 swordState = "ready" # ready / throw
 
 def player(x, y):
-    screen.blit(playerImg, (x, y)) # rysuje gracza
+    screen.blit(playerImg, (x, y))  # rysuje gracza
+    # r = screen.blit(playerImg, (x, y)) # rysuje gracza i dodaje do zmiennej "r"
+    # global dirtyRect # odwołanie do zmiennej poza funkcją
+    # dirtyRect.append(r) # dodajemy zmienną do listy odświeżania
 
 def enemy(x, y):
-    screen.blit(enemyImg, (x, y)) # rysuje przeciwnika 1
+    screen.blit(enemyImg, (x, y))  # rysuje przeciwnika 1
+    # r = screen.blit(enemyImg, (x, y)) # rysuje przeciwnika 1
+    # global dirtyRect  # odwołanie do zmiennej poza funkcją
+    # dirtyRect.append(r)  # dodajemy zmienną do listy odświeżania
 
 def throw_sword(x, y):
     global swordState # odwołanie do zmiennej poza funkcją
     swordState = "throw"
-    screen.blit(swordImg, (x + 16, y + 10)) # rysuje miecz
+    screen.blit(swordImg, (x + 16, y + 10))  # rysuje miecz
+    # r = screen.blit(swordImg, (x + 16, y + 10)) # rysuje miecz
+    # global dirtyRect  # odwołanie do zmiennej poza funkcją
+    # dirtyRect.append(r)  # dodajemy zmienną do listy odświeżania
+
+def is_collision(enemyX, enemyY, swordX, swordY):
+    distance = math.sqrt(math.pow(enemyX - swordX, 2) + math.pow(enemyY - swordY, 2)) # wzór na odległość punktów
+    if distance < 25:
+        return True
+    else:
+        return False
+def gen_enemy():
+    global enemyX, enemyY, enemySpeedX
+    enemyX = random.randint(1, 735)
+    enemyY = 0
+    enemySpeedX = random.choice([-3, -2, -1, 1, 2, 3])
+
 
 running = True
 while running:
@@ -69,14 +97,14 @@ while running:
     speedY = 0
 
     if keys[pygame.K_LEFT]:
-        speedX = -3
+        speedX = -playerSpeedChange
     elif keys[pygame.K_RIGHT]:
-        speedX = 3
+        speedX = playerSpeedChange
 
     if keys[pygame.K_UP]:
-        speedY = -3
+        speedY = -playerSpeedChange
     elif keys[pygame.K_DOWN]:
-        speedY = 3
+        speedY = playerSpeedChange
 
     playerX += speedX
     playerY += speedY
@@ -111,7 +139,16 @@ while running:
         throw_sword(swordX,swordY)
         swordY -= swordSpeedY
 
+    # kolizja
+    collision = is_collision(enemyX, enemyY, swordX, swordY)
+    if collision:
+        swordState = "ready" # resetujemy miecz
+        swordY = -50 # i usuwamy go z ekranu
+        score += 1 # dodajemy punkt
+        gen_enemy()
+
     enemy(enemyX, enemyY)
 
-    pygame.display.flip() # odswierza caly ekran (wcześnie było update)
-    clock.tick(60) # poprawne odtwarzanie w 60 klatkach na sekunde
+    pygame.display.flip() # odświeża caly ekran
+    # pygame.display.update(dirtyRect) # odświeża konkretne obszary dodane do zmiennej (listy) - nie działa na openGL
+    clockGame.tick(60) # poprawne odtwarzanie w 60 klatkach na sekunde
